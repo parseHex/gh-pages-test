913ad1bf2c76f70a77d43979a8aa3ee6f1609c40
Thomas <1779937+parseHex@users.noreply.github.com>
2018-09-23 21:19:27 -0400

    move deploy to command instead of build script

--- build.js
+++ build.js
@@ -1,14 +1,10 @@
-const fs = require('fs');
-const ghpages = require('gh-pages');
 const copy = require('copy')
 
-const BUILD_PATH = 'dist';
+const BUILD_PATH = 'dist/';
 
 (async () => {
-	await cp('src/main.js', BUILD_PATH);
+	await cp('src/*.js', BUILD_PATH);
 	await cp('index.html', BUILD_PATH);
-
-	ghpages.publish(BUILD_PATH, function (err) { });
 })();
 
 function cp(from, to) {
